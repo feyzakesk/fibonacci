3146a6da63c93001fdf1b97c2cae6238f88cdf89
feyzakesk <feyzaaa3455@gmail.com>
2023-03-02 00:06:35 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int element, first = 0, second = 1;
+
+        System.out.print("Fibonacci serisinin eleman sayısı: ");
+        element = input.nextInt();
+
+        for (int i = 1; i <= element + 1; i++) {
+            System.out.print(first + " ");
+            int total = first + second;
+            first = second;
+            second = total;
+        }
+    }
+}
